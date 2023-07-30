8dff061217540ba0b2ec2720758c7300c786398b
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-07-30 19:18:24 +0530

    Created elements for calculator

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,27 @@ function App() {
 
   return (
     <>
-      <div>Calculator</div>
+      <div id='calculator'>
+        <div id='formula'></div>
+        <div id='display'></div>
+        <div id="clear">AC</div>
+        <div id="divide">/</div>
+        <div id="multiply">x</div>
+        <div id="seven">7</div>
+        <div id="eight">8</div>
+        <div id="nine">9</div>
+        <div id="subtract">-</div>
+        <div id="four">4</div>
+        <div id="five">5</div>
+        <div id="six">6</div>
+        <div id="add">+</div>
+        <div id="one">1</div>
+        <div id="two">2</div>
+        <div id="three">3</div>
+        <div id="equals">=</div>
+        <div id="zero">0</div>
+        <div id="decimal">.</div>
+      </div>
     </>
   );
 }
