128f8a30df2dd406ae629e12dfc0921547a9b4a0
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-07-31 09:25:00 +0530

    Added basic style

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,25 @@ function App() {
   return (
     <>
       <div id='calculator'>
-        <div id='formula'></div>
-        <div id='display'></div>
-        <div id="clear">AC</div>
-        <div id="divide">/</div>
-        <div id="multiply">x</div>
-        <div id="seven">7</div>
-        <div id="eight">8</div>
-        <div id="nine">9</div>
-        <div id="subtract">-</div>
-        <div id="four">4</div>
-        <div id="five">5</div>
-        <div id="six">6</div>
-        <div id="add">+</div>
-        <div id="one">1</div>
-        <div id="two">2</div>
-        <div id="three">3</div>
-        <div id="equals">=</div>
-        <div id="zero">0</div>
-        <div id="decimal">.</div>
+        <div className='screen' id='formula'></div>
+        <div className='screen' id='display'></div>
+        <div className='key' id="clear">AC</div>
+        <div className='operator-key key' id="divide">/</div>
+        <div className='operator-key key' id="multiply">x</div>
+        <div className='numeric-key key' id="seven">7</div>
+        <div className='numeric-key key' id="eight">8</div>
+        <div className='numeric-key key' id="nine">9</div>
+        <div className='operator-key key' id="subtract">-</div>
+        <div className='numeric-key key' id="four">4</div>
+        <div className='numeric-key key' id="five">5</div>
+        <div className='numeric-key key' id="six">6</div>
+        <div className='operator-key key' id="add">+</div>
+        <div className='numeric-key key' id="one">1</div>
+        <div className='numeric-key key' id="two">2</div>
+        <div className='numeric-key key' id="three">3</div>
+        <div className='key' id="equals">=</div>
+        <div className='numeric-key key' id="zero">0</div>
+        <div className='numeric-key key' id="decimal">.</div>
       </div>
     </>
   );
